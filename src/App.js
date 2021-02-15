56deb6c94ec50bd369a7cdde1a636c11be7e2d9c
Ankit <panchalankit07@gmail.com>
2021-02-15 13:41:50 +0530

    routing chnages for gh pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="wrapper">
-      <Router>
+      <Router basename={`${process.env.PUBLIC_URL}/`}>
         <Sidebar />
         <Main />
       </Router>
